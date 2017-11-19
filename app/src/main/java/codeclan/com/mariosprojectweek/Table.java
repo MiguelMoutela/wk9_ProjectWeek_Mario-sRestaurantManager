318e3ebb45ce10b22c639fc68c97177f707af13d
Miguel <miguelmoutela@hotmail.com>
2017-11-19 14:17:57 +0000

    boiler plate

---
+++ app/src/main/java/codeclan/com/mariosprojectweek/Table.java
@@ -0,0 +1,13 @@
+package codeclan.com.mariosprojectweek;
+
+/**
+ * Created by user on 19/11/2017.
+ */
+
+public class Table {
+    private Patronal[] table;
+
+    public Table() {
+        this.table = new Patronal[2];
+    }
+}
