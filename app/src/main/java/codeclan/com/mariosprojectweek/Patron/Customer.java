cee5b835c1bf8ddfa712e44241b36e1ecab0cee7
Miguel <miguelmoutela@hotmail.com>
2017-11-19 14:05:06 +0000

    boiler plate

--- app/src/main/java/codeclan/com/mariosprojectweek/Patron/Customer.java
+++ app/src/main/java/codeclan/com/mariosprojectweek/Patron/Customer.java
@@ -1,10 +1,21 @@
 package codeclan.com.mariosprojectweek.Patron;
 
+import java.util.ArrayList;
+
 import codeclan.com.mariosprojectweek.Patronal;
+import codeclan.com.mariosprojectweek.Sellable;
 
 /**
  * Created by user on 19/11/2017.
  */
 
 public class Customer implements Patronal {
+    private ArrayList<Sellable>order;
+
+    public Customer(ArrayList Order) {
+        this.order = new ArrayList<>();
+    }
+
+    public float getBill() {}
+
 }
--- app/src/main/java/codeclan/com/mariosprojectweek/Patron/Regular.java
+++ app/src/main/java/codeclan/com/mariosprojectweek/Patron/Regular.java
@@ -1,10 +1,18 @@
 package codeclan.com.mariosprojectweek.Patron;
 
+import java.util.ArrayList;
+
 import codeclan.com.mariosprojectweek.Patronal;
+import codeclan.com.mariosprojectweek.Sellable;
 
 /**
  * Created by user on 19/11/2017.
  */
 
 public class Regular implements Patronal {
+    private ArrayList<Sellable>order;
+
+    public Regular(ArrayList order) {
+        this.order = new ArrayList<>();
+    }
 }
