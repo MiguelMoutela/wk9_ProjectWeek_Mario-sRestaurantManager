c3fad453e162e82b11efbc88f2e61835e0fb604c
Miguel <miguelmoutela@hotmail.com>
2017-11-19 14:24:13 +0000

    boiler plate

---
+++ app/src/main/java/codeclan/com/mariosprojectweek/Restaurant.java
@@ -0,0 +1,21 @@
+package codeclan.com.mariosprojectweek;
+
+import codeclan.com.mariosprojectweek.Menu.Menu;
+
+/**
+ * Created by user on 19/11/2017.
+ */
+
+public class Restaurant {
+    private String name;
+    private float turnover;
+    private Table[] tables;
+    private Menu menu;
+
+    public Restaurant () {
+        this.name = name;
+        this.turnover = turnover;
+        this.tables = new Table[5];
+        this.menu = menu;
+    }
+}
